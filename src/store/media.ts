a1125ffdc03c7a31958e1c153820a42d48ab4ac3
Sean Wambua <seanwambua@gmail.com>
2026-01-09 19:05:43 +0000

    Use persitent local storage for uploads

--- src/store/media.ts
+++ src/store/media.ts
@@ -1,4 +1,6 @@
+'use client';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { placeholderImages } from '@/lib/placeholder-images';
 
 interface MediaState {
@@ -8,9 +10,16 @@ interface MediaState {
   setLogo: (logoUrl: string) => void;
 }
 
-export const useMediaStore = create<MediaState>((set) => ({
-  heroImage: placeholderImages.hero.imageUrl,
-  setHeroImage: (imageUrl) => set({ heroImage: imageUrl }),
-  logo: null,
-  setLogo: (logoUrl) => set({ logo: logoUrl }),
-}));
+export const useMediaStore = create(
+  persist<MediaState>(
+    (set) => ({
+      heroImage: placeholderImages.hero.imageUrl,
+      setHeroImage: (imageUrl) => set({ heroImage: imageUrl }),
+      logo: null,
+      setLogo: (logoUrl) => set({ logo: logoUrl }),
+    }),
+    {
+      name: 'media-storage', // name of the item in the storage (must be unique)
+    }
+  )
+);
