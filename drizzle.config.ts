17a820c9d6febcb2c69bf578ec677575557cd592
Sean Wambua <seanwambua@gmail.com>
2026-01-17 16:38:42 +0000

    Error running npm run reinitialize

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,13 @@
 
 import type { Config } from "drizzle-kit";
-import "dotenv/config";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL environment variable is not set.');
-}
+const dbUrl = process.env.DATABASE_URL || "sqlite.db";
 
 export default {
   schema: "./src/lib/db/schema.ts",
   out: "./drizzle",
   driver: "better-sqlite",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: dbUrl,
   },
 } satisfies Config;
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,9 +2,7 @@ import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import * as schema from './schema';
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL environment variable is not set or is empty');
-}
+const dbUrl = process.env.DATABASE_URL || "sqlite.db";
 
-const sqlite = new Database(process.env.DATABASE_URL);
+const sqlite = new Database(dbUrl);
 export const db = drizzle(sqlite, { schema, logger: true });
