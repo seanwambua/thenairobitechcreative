9662b22379ad15bc8c6f40dcbb070637c4c7e8aa
Sean Wambua <seanwambua@gmail.com>
2026-01-27 23:39:05 +0000

    doesnt work

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -7,7 +7,23 @@ import { X } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 
-const Sheet = SheetPrimitive.Root;
+interface SheetContextProps {
+  modal?: boolean;
+}
+
+const SheetContext = React.createContext<SheetContextProps>({
+  modal: true,
+});
+
+const Sheet = ({
+  modal = true,
+  ...props
+}: SheetPrimitive.DialogProps & SheetContextProps) => (
+  <SheetContext.Provider value={{ modal }}>
+    <SheetPrimitive.Root modal={modal} {...props} />
+  </SheetContext.Provider>
+);
+Sheet.displayName = 'Sheet';
 
 const SheetTrigger = SheetPrimitive.Trigger;
 
@@ -50,29 +66,31 @@ const sheetVariants = cva(
 );
 
 interface SheetContentProps
-  extends
-    React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
+  extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
     VariantProps<typeof sheetVariants> {}
 
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = 'right', className, children, ...props }, ref) => (
-  <SheetPortal>
-    <SheetOverlay />
-    <SheetPrimitive.Content
-      ref={ref}
-      className={cn(sheetVariants({ side }), className)}
-      {...props}
-    >
-      {children}
-      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-        <X className="h-4 w-4" />
-        <span className="sr-only">Close</span>
-      </SheetPrimitive.Close>
-    </SheetPrimitive.Content>
-  </SheetPortal>
-));
+>(({ side = 'right', className, children, ...props }, ref) => {
+  const { modal } = React.useContext(SheetContext);
+  return (
+    <SheetPortal>
+      {modal && <SheetOverlay />}
+      <SheetPrimitive.Content
+        ref={ref}
+        className={cn(sheetVariants({ side }), className)}
+        {...props}
+      >
+        {children}
+        <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+          <X className="h-4 w-4" />
+          <span className="sr-only">Close</span>
+        </SheetPrimitive.Close>
+      </SheetPrimitive.Content>
+    </SheetPortal>
+  );
+});
 SheetContent.displayName = SheetPrimitive.Content.displayName;
 
 const SheetHeader = ({
